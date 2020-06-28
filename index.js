f58292b1f0abb823775f8693e73d80875117e2b5
rlimax <rodrigoetb@gmail.com>
2020-06-27 22:29:49 -0300

    Ampliação de escopo.

--- index.js
+++ index.js
@@ -2,15 +2,61 @@ const request = require('request-promise');
 
 const cheerio = require('cheerio');
 
-const URL = 'https://ptax.bcb.gov.br/ptax_internet/consultarUltimaCotacaoDolar.do';
+//const URL = 'https://ptax.bcb.gov.br/ptax_internet/consultarUltimaCotacaoDolar.do';
+const URL2 = 'https://www.estantevirtual.com.br/livreiros/casaraodlivros'
 
 async function acesso(){
-    const response = await request(URL);
+    const response = await request(URL2);
     let $ = cheerio.load(response);
-    let dolar = $('td[align=right]').first().text();
-    let data = $('td[align=CENTER]').text();
-    console.log(`Hoje ${data}, e o valor do dólar é de $${dolar}`);
-    //console.log(data);
-    //console.log(dolar);
+
+
+    let nomelivro = $('h2[itemprop=name]').text();
+    nomelivro = nomelivro.split("\n              ");
+    
+    for(let i=0; i<nomelivro.length; i++){
+        if(nomelivro[i]==''){
+            nomelivro.splice(i,1);
+        }
+    }
+    for(let i=0; i<nomelivro.length; i++){
+       nomelivro[i] = nomelivro[i].replace("  ","");
+    }
+    //console.log(nomelivro);
+
+    let ano = $('div[class=ano-editora]').text().split("Ano: ");
+    for(let i=0; i<ano.length; i++){
+        if(ano[i]==""){
+            ano.splice(i,1);
+        }
+    }
+    //console.log(ano);
+
+   let preco = $('p[class=preco_cifrao]').text().split("R$ ");
+   for(let i=0; i<preco.length; i++){
+       if(preco[i]==""){
+           preco.splice(i,1);
+       }
+   }
+   //console.log(preco);
+
+   for (let i=0; i<ano.length; i++){
+    console.log(`Indice: ${i+1} | Valor: ${preco[i]} | Livro: ${nomelivro[i]} - Ano: ${ano[i]}`);
+   }
+
+   capaList = [];
+   let capa = $('div[class=exemplar]').each(
+       function(){
+            a = $(this);
+            let src = a.find('img').attr('data-src');
+            capaList.push(src);
+            //console.log(`imagem = ${src}`);
+       }
+   );
+   //console.log(capalist);
+   for (let i=0; i<ano.length; i++){
+    console.log(`Indice: ${i+1} | Valor: ${preco[i]} | Livro: ${nomelivro[i]} - Ano: ${ano[i]}`);
+    console.log(`imagem: ${capaList[i]}`);
+   }
+
 }
 acesso();
